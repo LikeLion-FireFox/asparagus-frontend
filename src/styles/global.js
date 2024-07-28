6be2f9f58ef038fcdb8959bbeaf60f08ccb07efc
“2NNS-V” <msy3824@knu.ac.kr>
2024-07-28 15:04:56 +0900

    fix:globalStyle에서 @import CSS 에러 해결

--- src/styles/global.js
+++ src/styles/global.js
@@ -1,12 +1,10 @@
 import { createGlobalStyle } from "styled-components";
 
 export const GlobalStyle = createGlobalStyle`
-  @import url('https://cdn.jsdelivr.net/gh/orioncactus/pretendard/dist/web/static/pretendard.css');
-  
   body {
     margin: 0;
     padding: 0;
-    letter-spacing : -1px;
+    letter-spacing: -1px;
     font-family: 'Pretendard', sans-serif;
   }
 `;
